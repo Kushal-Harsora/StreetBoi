c540643d377aa63cf98994b40c4c4452709ee708
Kushal Harsora <kushalharsora13@gmail.com>
2025-07-07 15:19:03 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <main className=' h-screen w-screen flex items-center justify-center'>
+        Hello
+      </main>
+    </React.Fragment>
+  )
+}
+
+export default App
